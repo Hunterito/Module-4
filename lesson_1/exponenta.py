0835ecc33403c7c3fb4861822dd599529a32eca8
Hunterito <120974987+Hunterito@users.noreply.github.com>
2023-06-25 12:39:26 +0300

    Delete lesson_1 directory

--- lesson_1/exponenta.py
+++
@@ -1,7 +0,0 @@
-#def func(string: str) -> bool:
-#    return True if string == string[-1::-1] else False
-
-func = lambda string: True if string == string[-1::-1] else False
-
-
-print(func(input()))
